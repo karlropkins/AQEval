7a87547168cbaf55700652e5ba9aded83fa41adf
karlropkins <k.ropkins@its.leeds.ac.uk>
2026-01-15 11:16:37 +0000

    tinytest issue

--- NEWS.md
+++ NEWS.md
@@ -6,7 +6,8 @@
     * __v 0.6.1__ [2025-02-07] added/testing use.bam option to isolateContribution (might not be staying) 
     * __v 0.6.2__ [2025-07-12] updated to date handling in calcDateRangeStat; added tinytest package testing 
     * __v 0.6.3__ [2025-11-15] find.break typo fixed 
-    * __v 0.6.4__ [2025-12-07] findNearLatLon update; option to remove duplicates in ref
+    * __v 0.6.4__ [2025-12-07] findNearLatLon update; option to remove duplicates in ref 
+    * __v 0.6.5__ [2025-01-15] test update for ggplot class change  
  
 # AQEval version 0.5
 
--- inst/tinytest/test_AQEval_quant.R
+++ inst/tinytest/test_AQEval_quant.R
@@ -5,11 +5,14 @@ if(at_home()){
   suppressMessages(ans <- quantBreakPoints(temp, "no2", h=0.15, show="nothing"))
   expect_equal(names(ans), c("data", "breaks", "data2", "plot", "report", "model"))
   expect_equal(ans$breaks$bpt[1], 211)
-  expect_equal(class(ans$plot), c("gg", "ggplot"))
+  # ggplot S3/4/7 ?
+  #expect_equal(class(ans$plot), c("gg", "ggplot"))
+  expect_true(is_ggplot(ans$plot))
 
   suppressMessages(ans <- quantBreakSegments(temp, "no2", h=0.15, show="nothing"))
   expect_equal(names(ans), c("data", "segments", "data2", "plot", "report", "model"))
   expect_equal(ans$segments$seg.str[2], 209)
-  expect_equal(class(ans$plot), c("gg", "ggplot"))
+  #expect_equal(class(ans$plot), c("gg", "ggplot"))
+  expect_true(is_ggplot(ans$plot))
 
 }
